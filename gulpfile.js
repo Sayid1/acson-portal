652f9736f2fd98d6425ef0b5aac90bb88626abc1
郑猛 <zhengmeng@ek56.com>
2024-06-15 11:58:24 +0800

    615

--- gulpfile.js
+++ gulpfile.js
@@ -25,10 +25,6 @@ gulp.task("watch", function () {
   gulp.watch(["src/*.html", "partials/*.html"], gulp.series("fileinclude"));
 });
 
-// 清空 dist 目录任务
-// gulp.task("clean", function () {
-//   return gulp.src("dist", { read: false, allowEmpty: true }).pipe(clean());
-// });
 function cleanDist() {
   // body omitted
   return gulp.src("dist", { read: false, allowEmpty: true }).pipe(clean());
@@ -41,34 +37,40 @@ function move() {
 
 // 定义一个任务用于处理 JS 文件
 function js() {
-  return gulp
-    .src("dist/assets/js/**/*.js") // 匹配 assets/js 目录及其子目录下的所有 JS 文件
-    .pipe(sourcemaps.init()) // 初始化 sourcemaps
-    .pipe(uglify()) // 压缩 JS 文件
-    .pipe(sourcemaps.write(".")) // 写入 sourcemaps
-    .pipe(gulp.dest("dist/js")); // 输出到 dist/js 目录
+  return (
+    gulp
+      .src("dist/assets/js/**/*.js") // 匹配 assets/js 目录及其子目录下的所有 JS 文件
+      .pipe(sourcemaps.init()) // 初始化 sourcemaps
+      // .pipe(uglify()) // 压缩 JS 文件
+      .pipe(sourcemaps.write(".")) // 写入 sourcemaps
+      .pipe(gulp.dest("dist/js"))
+  ); // 输出到 dist/js 目录
 }
 
 // 定义一个任务用于处理 CSS 文件
 function css() {
-  return gulp
-    .src("dist/assets/css/**/*.css") // 匹配 assets/css 目录及其子目录下的所有 CSS 文件
-    .pipe(sourcemaps.init()) // 初始化 sourcemaps
-    .pipe(cleanCSS()) // 压缩 CSS 文件
-    .pipe(sourcemaps.write(".")) // 写入 sourcemaps
-    .pipe(gulp.dest("dist/css")); // 输出到 dist/css 目录
+  return (
+    gulp
+      .src("dist/assets/css/**/*.css") // 匹配 assets/css 目录及其子目录下的所有 CSS 文件
+      .pipe(sourcemaps.init()) // 初始化 sourcemaps
+      // .pipe(cleanCSS()) // 压缩 CSS 文件
+      .pipe(sourcemaps.write(".")) // 写入 sourcemaps
+      .pipe(gulp.dest("dist/css"))
+  ); // 输出到 dist/css 目录
 }
 function html() {
-  return gulp
-    .src(["src/*.html"])
-    .pipe(
-      fileInclude({
-        prefix: "@@",
-        basepath: "@file",
-      })
-    )
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("dist"));
+  return (
+    gulp
+      .src(["src/*.html"])
+      .pipe(
+        fileInclude({
+          prefix: "@@",
+          basepath: "@file",
+        })
+      )
+      // .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest("dist"))
+  );
 }
 
 gulp.task("build", gulp.series(cleanDist, move, gulp.parallel(js, css, html)));
